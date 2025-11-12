48ed0565f981c5436a89f87a086d2e6045b1c408
camka14 <camka14@gmail.com>
2025-11-12 10:43:34 -0800

    Refactor: Remove duplicate function in `InputFile.android.kt`
    This commit removes a duplicated `guessMimeType` function declaration within `shared/src/androidMain/kotlin/io/appwrite/models/InputFile.android.kt`.

--- shared/src/androidMain/kotlin/io/appwrite/models/InputFile.android.kt
+++ shared/src/androidMain/kotlin/io/appwrite/models/InputFile.android.kt
@@ -1,20 +1,16 @@
 package io.appwrite.models
 
 import android.os.Build
-import androidx.annotation.RequiresApi
+import java.io.File
 import java.net.URLConnection
 import java.nio.file.Files
-import java.io.File
 
 actual fun guessMimeType(input: String): String {
     val file = File(input)
-    actual fun guessMimeType(input: String): String {
-        val file = File(input)
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Files.probeContentType(file.toPath()) ?: 
-                URLConnection.guessContentTypeFromName(file.name) ?: ""
-        } else {
-            URLConnection.guessContentTypeFromName(file.name) ?: ""
-        }
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        Files.probeContentType(file.toPath()) ?: URLConnection.guessContentTypeFromName(file.name)
+        ?: ""
+    } else {
+        URLConnection.guessContentTypeFromName(file.name) ?: ""
     }
 }
